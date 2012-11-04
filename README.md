aff151ac24efa7187bf85c76f54f38d4898e5105
rhasson <rhasson@gmail.com>
2012-11-04 08:47:58 -0500

    initial commit:

---
+++ README.md
@@ -0,0 +1,2 @@
+#Poll Location finder based on Google civic API data
+##Enter home address and get a list of polling locations
---
+++ server.js
@@ -0,0 +1,49 @@
+var GOOG_KEY = "AIzaSyAMXdydx9lk6d6gLZ92RX3RfvKKSz0-O54";
+var ELECTION_ID = 4000;
+var GOOG_URL = "https://www.googleapis.com/civicinfo/us_v1";
+
+var r = require('request'),
+	Express = require('express'),
+	jade = require('jade'),
+	server = Express.createServer();
+
+server.set('views', __dirname + '/public/views');
+server.set('view engine', 'jade');
+
+server.use(Express.logger());
+server.use(Express.bodyParser());
+server.use(Express.methodOverride());
+server.use(Express.static(__dirname + '/public'));
+
+server.post('/poll', function(req, res) {
+	console.log('here', req.body)
+	if (req.body.address !== 'null' || req.body.address !== '') {
+		console.log('inside')
+
+		var url = GOOG_URL + '/voterinfo/'+ELECTION_ID+'/lookup?key='+GOOG_KEY
+
+		var resp_body = {};
+
+		r({method: 'POST', url: url, json: {address: req.body.address}}, function(err, resp, body) {
+			console.log(body)
+			if (body.status === 'success') {
+				resp_body.election = body.election.name;
+				resp_body.date = body.election.electionDay;
+				resp_body.pollingLocations = body.pollingLocations;
+
+				res.send(resp_body);
+			} else {
+				console.log('failed')
+				res.send({error: 'Failed to get polling location information'});
+			}
+		});
+	} else {
+		res.send({error: 'Failed to get polling location information'});
+	}
+});
+
+server.get('/', function(req, res) {
+	res.render('layout');
+});
+
+server.listen(8000);
